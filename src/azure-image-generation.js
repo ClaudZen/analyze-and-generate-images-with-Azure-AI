b0570e72e9797b27d493114f00ccfb344397eee6
Claudio Anticoi <55770366+ClaudZen@users.noreply.github.com>
2023-11-26 20:21:51 +0000

    Add openai package and azure-image-generation module

---
+++ src/azure-image-generation.js
@@ -0,0 +1,22 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const organization = process.env.REACT_APP_API_ORGANIZATION_OPEN_AI;
+const key = process.env.REACT_APP_API_KEY_OPEN_AI;
+
+export async function generateImage(text) {
+    console.log(key);
+    const configuration = new Configuration({
+        organization: organization,
+        apiKey: key,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createImage({
+        model: "dall-e-3",
+        prompt: text,
+        n: 1,
+        size: "1024x1024",
+    });
+
+    return { "prompt": text, "URL": response.data.data[0].url };
+}
